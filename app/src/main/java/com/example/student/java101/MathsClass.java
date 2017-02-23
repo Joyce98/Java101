87bf3708459c90dfcb5338c8d753e3dc035549c0
leilahassan <hassanleila333@gmail.com>
2017-02-23 10:40:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/student/java101/MathsClass.java
@@ -0,0 +1,26 @@
+package com.example.student.java101;
+
+/**
+ * Created by Student on 2/18/2017.
+ */
+
+public class MathsClass {
+    //Declaring variables.
+    private int age;
+    private int hours;
+    private double shoeSize;
+    private String firstName;
+    private String Address;
+
+    //Initializing variables
+    private int sum = 40;
+    private int classHours = 8;
+    private String homeAddress = "Eastliegh";
+
+}
+
+
+
+
+
+
